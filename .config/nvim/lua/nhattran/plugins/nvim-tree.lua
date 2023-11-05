a0236de9c26bfe462130ea7127df6a82f4b69a67
Nhat Tran <nhattran@Nhats-MacBook-Pro.local>
2023-11-05 14:04:04 +0700

    dev-env: Update nvim-tree keymapping

--- .config/nvim/lua/nhattran/plugins/nvim-tree.lua
+++ .config/nvim/lua/nhattran/plugins/nvim-tree.lua
@@ -49,6 +49,7 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end,
 }
