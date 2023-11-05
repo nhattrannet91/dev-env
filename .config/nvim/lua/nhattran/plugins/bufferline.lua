fb705c30dfbfb515667fd93d11c400a4bf68230b
Nhat Tran <nhattran@Nhats-MacBook-Pro.local>
2023-11-05 13:58:48 +0700

    dev-env: Refine bufferline with offset

--- .config/nvim/lua/nhattran/plugins/bufferline.lua
+++ .config/nvim/lua/nhattran/plugins/bufferline.lua
@@ -1,11 +1,17 @@
-return {
-  "akinsho/bufferline.nvim",
+return { "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
   opts = {
     options = {
       mode = "tabs",
       separator_style = "slant",
+      offsets = {
+        {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+        }
+      },
     },
   },
 }
