8d8b034f7a609b5750ae8045d1f52cb560a97d4b
Nhat Tran <nhattran@Nhats-MacBook-Pro.local>
2023-11-05 14:09:57 +0700

    dev-env: Add comment plugin

---
+++ .config/nvim/lua/nhattran/plugins/comment.lua
@@ -0,0 +1,13 @@
+return {
+  "numToStr/Comment.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+  },
+  config = function()
+    -- import comment plugin safely
+    local comment = require("Comment")
+
+    -- enable comment
+    comment.setup()
+  end,
+}
