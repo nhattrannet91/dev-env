8b65fb58d788d12eaca7797bd2f0492d92269e2f
Nhat Tran <nhattran@Nhats-MacBook-Pro.local>
2023-11-15 22:31:37 +0700

    dev-env: Add formatting config

--- .config/nvim/lua/nhattran/lazy.lua
+++ .config/nvim/lua/nhattran/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "nhattran.plugins" } }, {
+require("lazy").setup({ { import = "nhattran.plugins" }, { import = "nhattran.plugins.lsp" } }, {
   install = {
     colorscheme = { "nightfly" },
   },
