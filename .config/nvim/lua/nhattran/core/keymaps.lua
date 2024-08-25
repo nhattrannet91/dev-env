431f67645330b1b6b2cea9985e5eaa494b0a919e
Nhat Tran <nhat.tran@elca.vn>
2024-08-25 10:53:51 +0700

    dev-env: nvim: Refine keymaps with toggle search highlight & close other buffers

--- .config/nvim/lua/nhattran/core/keymaps.lua
+++ .config/nvim/lua/nhattran/core/keymaps.lua
@@ -9,9 +9,15 @@ local keymap = vim.keymap -- for conciseness
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+-- buffers
+keymap.set("n", "<leader>bX", ":Neotree action=close<bar>bufdo bd<CR>", { desc = "Close all buffers" }) -- close current split window
 
