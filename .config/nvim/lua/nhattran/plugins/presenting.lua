684e9fef0c0bfc23c13b75d38566083a61966caa
Nhat Tran <nhat.tran@elca.vn>
2025-04-08 18:36:27 +0700

    dev-env: nvim: Add plugins required to present

---
+++ .config/nvim/lua/nhattran/plugins/presenting.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "sotte/presenting.nvim",
+    dependencies = { 'MeanderingProgrammer/render-markdown.nvim' },
+    opts = {
+      -- fill in your options here
+      -- see :help Presenting.config
+    },
+    cmd = { "Presenting" },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  }
+}
